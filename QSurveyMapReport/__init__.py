6e681e9ee7518fae1311acfbf0c28c8557b2c67a
onoderra-g <ga19981023ku@gmail.com>
2024-11-15 22:48:41 +0900

    test

---
+++ QSurveyMapReport/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ Sample
+                                 A QGIS plugin
+ QGIS Sample Plugin
+                              -------------------
+        begin                : 2018-12-23
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by Yasunori Kirimoto
+        email                : contact@day-journal.com
+        license              : GNU General Public License v2.0
+ ***************************************************************************/
+"""
+
+
+def classFactory(iface):
+    from .Menu_Button import Sample
+    return Menu_Button(iface)
